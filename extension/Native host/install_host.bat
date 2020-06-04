273e77d307925409c23223e2281a7a51baca5db0
bhushanblaster <55149243+bhushanblaster@users.noreply.github.com>
2020-06-05 00:28:28 +0530

    Update install_host.bat

--- extension/Native host/install_host.bat
+++ extension/Native host/install_host.bat
@@ -3,6 +3,6 @@
 :: Change HKCU to HKLM if you want to install globally.
 :: %~dp0 is the directory containing this bat script and ends with a backslash.
 
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.dolby.native_messaging_host" /ve /t REG_SZ /d "%~dp0manifest.json" /f
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.noteit.native_messaging_host" /ve /t REG_SZ /d "%~dp0manifest.json" /f
 
 %SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe DocProcessor.dll /codebase /u
